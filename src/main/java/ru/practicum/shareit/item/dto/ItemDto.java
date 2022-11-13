162970c777ce1bb7bcb617ec4791488209c1c1a8
laserdi <v013v@ya.ru>
2022-11-13 18:29:08 +0200

    Sprint14 Final. Done.

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -11,5 +11,6 @@ public class ItemDto {
     String description; //Описание вещи.
     Long ownerId;       //ID хозяина вещи.
     Boolean available;  //Статус для бронирования: свободна, занята.
+    Boolean isRequest;  //Вещь создана по запросу ищущего пользователя (True - да)?
     Set<Long> reviews;  //ID фидбеков на вещь.
 }
